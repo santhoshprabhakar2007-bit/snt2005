ec73a131a94e21d99d8c4ff23fdcd46cbc1254b5
santhosh123-tech <cse24590894@gmail.com>
2025-08-11 14:28:02 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
-
-
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 result = num1 + num2
 print("The sum is:", result)
+print(the resultis founded)
